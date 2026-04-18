43d7ba4aac35ccc2399ab1dbfbc4ede2ddad2c97
weimin96 <735313582@qq.com>
2026-04-18 20:35:45 +0800

    启动报错

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -25,10 +25,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_notification::init())
-        .plugin(tauri_plugin_autostart::init(
-            tauri_plugin_autostart::MacosLauncher::LaunchAgent,
-            Some(vec!["--hidden"]),
-        ))
+        .plugin(tauri_plugin_autostart::Builder::new().build())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
         .setup(|app| {
